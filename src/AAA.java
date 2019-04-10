342a6d8b10490c8881836eeb1178cfa3f1d73b89
lijm <lijm@szkingdom.com>
2019-04-10 09:21:56 +0800

    提交

---
+++ src/AAA.java
@@ -0,0 +1,27 @@
+/**
+ * <p>文件名称: AAA.java</p>
+ * <p>文件描述: </p>
+ * <p>版权所有: 版权所有(C)2016-</p>
+ * <p>公    司: 金证财富南京科技有限公司</p>
+ * <p>内容摘要: </p>
+ * <p>其他说明: </p>
+ * <p>创建日期： 2019/4/10 9:20 </p>
+ * <p>完成日期：</p>
+ * <p>修改记录1:</p>
+ * <pre>
+ *    修改日期：
+ *    版 本 号：
+ *    修 改 人：
+ *    修改内容：
+ * </pre>
+ * <p>修改记录2：…</p>
+ *
+ * @version 1.0
+ * @author lijm@szkingdom.com
+ */
+
+public class AAA {
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
